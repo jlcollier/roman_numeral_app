65d5a648a1f6932e24bc7ae81ff4a34552e49e3b
Jared Collier <jared.collier@jared.collier>
2025-01-20 14:52:31 -0700

    add additional AI solution for roman numeral problem

--- src/server/romanNumerals.js
+++ src/server/romanNumerals.js
@@ -59,3 +59,36 @@ function convertToRoman(n) {
 
     return romanStringParts.reverse().join('')
 }
+
+function aiToRoman(num) {
+    if (num < 1 || num > 3999) {
+        throw new Error("Input must be between 1 and 3999.");
+    }
+
+    const romanNumerals = [
+        { value: 1000, numeral: 'M' },
+        { value: 900, numeral: 'CM' },
+        { value: 500, numeral: 'D' },
+        { value: 400, numeral: 'CD' },
+        { value: 100, numeral: 'C' },
+        { value: 90, numeral: 'XC' },
+        { value: 50, numeral: 'L' },
+        { value: 40, numeral: 'XL' },
+        { value: 10, numeral: 'X' },
+        { value: 9, numeral: 'IX' },
+        { value: 5, numeral: 'V' },
+        { value: 4, numeral: 'IV' },
+        { value: 1, numeral: 'I' }
+    ];
+
+    let result = '';
+
+    for (let i = 0; i < romanNumerals.length; i++) {
+        while (num >= romanNumerals[i].value) {
+            result += romanNumerals[i].numeral;
+            num -= romanNumerals[i].value;
+        }
+    }
+
+    return result;
+}
