3b55b5c997fe45e08f25b25bdc19c03c52e75930
Jared Collier <jared.collier@jared.collier>
2025-01-27 03:58:18 -0700

    add better error handling in express file

--- src/server/main.js
+++ src/server/main.js
@@ -5,12 +5,21 @@ import { convertToRoman } from "./utilities.js"
 const app = express();
 
 app.get("/romannumeral", (req, res) => {
-  let romanNumeral = convertToRoman(+req.query.query)
+  //monitor endpoint call
+  let romanNumeral;
+  try {
+    romanNumeral = convertToRoman(+req.query.query)
+  } catch(err) {
+    //critical err
+    res.status(500).send()
+    return
+  }
 
   // If we receive a falsey value or 'error' string from convertToRoman,
   //   we assume the input from the front end was invalid
   if (!romanNumeral || romanNumeral === 'error') {
     res.status(400).send({
+      //error log: pay attention
       input: req.query.query,
       error: "Something was wrong with the input, try again" })
     return
