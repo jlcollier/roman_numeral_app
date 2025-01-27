12f5e4dd1a3d1f1de071be239d17d8b2fa4ff264
Jared Collier <jared.collier@jared.collier>
2025-01-27 01:13:16 -0700

    add inline comments to server/main.js

--- src/server/main.js
+++ src/server/main.js
@@ -1,12 +1,14 @@
 import express from "express";
 import ViteExpress from "vite-express";
-import { convertToRoman } from "./romanNumerals.js"
+import { convertToRoman } from "./utilities.js"
 
 const app = express();
 
 app.get("/romannumeral", (req, res) => {
   let romanNumeral = convertToRoman(+req.query.query)
 
+  // If we receive a falsey value or 'error' string from convertToRoman,
+  //   we assume the input from the front end was invalid
   if (!romanNumeral || romanNumeral === 'error') {
     res.status(400).send({
       input: req.query.query,
